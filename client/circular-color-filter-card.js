2b43c73af11c5ec2c08155d6a0cde7fe93fd63cf
Anwesh43 <anweshthecool0@gmail.com>
2017-05-20 18:35:53 +0530

    correcting syntax error

--- client/circular-color-filter-card.js
+++ client/circular-color-filter-card.js
@@ -39,50 +39,51 @@ class CircularCard {
             this.render()
         }
     }
-    class ColorFilter {
-        constructor(color,w,h,animEndCb) {
-            this.deg = 0
-            this.w = w
-            this.h = h
-            this.color = color
+}
+class ColorFilter {
+    constructor(color,w,h,animEndCb) {
+        this.deg = 0
+        this.w = w
+        this.h = h
+        this.color = color
+        this.dir = 0
+        this.animEndCb = animEndCb
+    }
+    draw(context,r) {
+        context.save()
+        context.translate(this.w/2,this.h/2)
+        context.rotate(this.deg*Math.PI/180)
+        context.beginPath()
+        for(var i = 0;i<=deg;i+=10) {
+            const x = r*Math.cos(i*Math.PI/180),y = r*Math.sin(i*Math.PI/180)
+            if(i == 0) {
+                context.moveTo(x,y)
+            }
+            else {
+                context.lineTo(x,y)
+            }
+        }
+        context.fillStyle = this.color
+        context.globalAlpha = 0.5
+        context.fill()
+        context.restore()
+    }
+    startMoving() {
+        if(this.dir == 0) {
+            if(this.deg == 0) {
+                this.dir = 1
+            }
+            else {
+                this.dir = -1
+            }
+            this.animEndCb()
+        }
+    }
+    update() {
+        this.deg += 30*this.dir
+        if(this.deg >= 360 || this.deg <= 0) {
             this.dir = 0
-            this.animEndCb = animEndCb
-        }
-        draw(context,r) {
-            context.save()
-            context.translate(this.w/2,this.h/2)
-            context.rotate(this.deg*Math.PI/180)
-            context.beginPath()
-            for(var i = 0;i<=deg;i+=10) {
-                const x = r*Math.cos(i*Math.PI/180),y = r*Math.sin(i*Math.PI/180)
-                if(i == 0) {
-                    context.moveTo(x,y)
-                }
-                else {
-                    context.lineTo(x,y)
-                }
-            }
-            context.fillStyle = this.color
-            context.globalAlpha = 0.5
-            context.fill()
-            context.restore()
-        }
-        startMoving() {
-            if(this.dir == 0) {
-                if(this.deg == 0) {
-                    this.dir = 1
-                }
-                else {
-                    this.dir = -1
-                }
-                this.animEndCb()
-            }
-        }
-        update() {
-            this.deg += 30*this.dir
-            if(this.deg >= 360 || this.deg <= 0) {
-                this.dir = 0
-            }
         }
     }
 }
+    
